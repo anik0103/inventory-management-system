778c40975c0c90dbc5799e5a3e002a04a0ccb45d
Daspriyabratya007 <your-email@example.com>
2025-07-02 11:17:52 +0530

    P added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Dashboard from './Dashbord/Dashboard';
-=======
-import Dashboard from './Components/Dashboard';
+// import Dashboard from './Dashbord/Dashboard';
+
+import Dashboard from './Components/Dashboard/Dashboard';
 // import SignIn from './Components/SignIn';
 // import SignUp from './Components/SignUp';
->>>>>>> 4b3a8db07fdc1fe7114cb7ae7b9c9fd121999927
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Dashboard/>
-=======
+
       {/* <SignIn/>
       <SignUp /> */}
       <Dashboard />
->>>>>>> 4b3a8db07fdc1fe7114cb7ae7b9c9fd121999927
+
     </div>
   );
 }
