4b3a8db07fdc1fe7114cb7ae7b9c9fd121999927
Anik <adhikaryanik70@gmail.com>
2025-07-02 11:03:11 +0530

    Comment out SignIn and SignUp imports and usage in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Components/Dashboard';
+// import SignIn from './Components/SignIn';
+// import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
+      {/* <SignIn/>
+      <SignUp /> */}
       <Dashboard />
     </div>
   );
