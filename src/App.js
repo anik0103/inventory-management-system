b6c4e9127b943429103b5f6da08e12b7c2fdadf8
Anik <adhikaryanik70@gmail.com>
2025-07-11 12:37:33 +0530

    Remove commented router code from App.js and clean up imports in SignIn.js

--- src/App.js
+++ src/App.js
@@ -2,42 +2,12 @@ import './App.css';
 import Dashboard from './Components/Dashbord/Dashboard'
 import SignIn from './Components/Authentication/SignIn';
 import SignUp from './Components/Authentication/SignUp';
-import { createBrowserRouter } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import Head from './Components/Product/Head';
 
 
 function App() {
-  // const appRouter= createBrowserRouter([
-  //   {
-  //     path:"/",
-  //    element:<Dashboard/>,
-  //    children:[
-  //     {
-  //       path:"/",
-  //       element:<Dashboard/>
-  //     },
-  //     {
-  //       path:"/My Orders",
-  //       element:<h1></h1>
-  //     },
-  //     {
-  //       path:"/Browse Products",
-  //       element:<h1></h1>
-  //     },
-  //     {
-  //       path:"/request Products",
-  //       element:<h1></h1>
-  //     },
-  //     {
-  //       path:"/Logout",
-  //       element:<h1></h1>
-  //     }
-  //    ]
-
-  //   }
-  // ])
   return (
     <div className="App">
       <Navbar/>
