7931a1d5fc57db21de8962943e632a7bea116fcd
Anik <adhikaryanik70@gmail.com>
2025-07-12 12:19:56 +0530

    Refactor Navbar component: enhance layout, improve dropdown functionality, and update styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,48 @@
 import SearchIcon from "../asset/search.png";
 import Cart from "../asset/shopping-cart.png";
-
+import theme from "../asset/themes.png";
+import man from "../asset/man.png";
+import location from "../asset/location.png";
+import { useState } from "react";
 const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggledropdown = () => {
+    setDropdown(!dropdown);
+  };
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-[#9dc3e6] w-full p-4 flex">
-      <div className="container-fluid flex space-x-40">
+    <nav className="w-full p-4 flex bg-[#9dc3e6]">
+      <div className="container-fluid flex space-x-40 items-center">
         <div className="flex items-center space-x-2">
           <img src={Cart} alt="logo" className="w-6" />
-          <h3>Alpha Inventory</h3>
+          <h3 className="font-semibold text-lg">Alpha Inventory</h3>
         </div>
-        <div className="align-items-center flex space-x-2">
+        <div className="flex items-center space-x-2">
           <input
-            className="form-control me-2 w-60 rounded-xl px-2"
+            className="form-control w-60 rounded-xl px-2 py-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn btn-outline-success" type="submit">
+          <button className="btn border border-green-500 text-green-500 rounded px-2 py-1 hover:bg-green-500 hover:text-white transition">
             <img src={SearchIcon} alt="search" className="w-4" />
           </button>
         </div>
       </div>
-      <div className="flex space-x-4 ml-auto items-center">
-        <p>Anik</p>
-        <p>Anik</p>
-        <p>rick</p>
+      <div className="flex space-x-8 ml-auto items-center relative">
+        <div className="flex align-middle">
+        <img src={location} alt="location" className="w-8 cursor-pointer" onClick={toggledropdown} />
+        <h1>kolkata</h1>
+        </div>
+        {dropdown && (
+          <ul className="absolute top-12 left-0 bg-gray-700 text-white list-none p-2 rounded shadow-lg z-50 min-w-[120px]">
+            <li className="py-1 px-3 hover:bg-gray-600 cursor-pointer">kolkata</li>
+            <li className="py-1 px-3 hover:bg-gray-600 cursor-pointer">kolkata</li>
+            <li className="py-1 px-3 hover:bg-gray-600 cursor-pointer">kolkata</li>
+          </ul>
+        )}
+        <img src={theme} alt="theme" className="w-8" />
+        <img src={man} alt="man" className="w-8" />
       </div>
     </nav>
   );
