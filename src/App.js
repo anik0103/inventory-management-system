8db401c12adadc165e65380d59549d90e3181451
briti <britisingh9@gmail.com>
2025-07-12 11:09:18 +0530

    Remove commented-out router code from App.js

--- src/App.js
+++ src/App.js
@@ -2,41 +2,12 @@ import './App.css';
 import Dashboard from './Components/Dashbord/Dashboard'
 import SignIn from './Components/Authentication/SignIn';
 import SignUp from './Components/Authentication/SignUp';
-import { createBrowserRouter } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import Head from './Components/Product/Head';
 import Filters from './Components/Product/Filters';
 
 function App() {
-  // const appRouter= createBrowserRouter([
-  //   {
-  //     path:"/",
-  //    element:<Dashboard/>,
-  //    children:[
-  //     {
-  //       path:"/",
-  //       element:<Dashboard/>
-  //     },
-  //     {
-  //       path:"/My Orders",
-  //       element:<h1></h1>
-  //     },
-  //     {
-  //       path:"/Browse Products",
-  //       element:<h1></h1>
-  //     },
-  //     {
-  //       path:"/request Products",
-  //       element:<h1></h1>
-  //     },
-  //     {
-  //       path:"/Logout",
-  //       element:<h1></h1>
-  //     }
-  //    ]
 
-  //   }
-  // ])
   return (
     <div className="App">
       <SignIn/>
