960cdbb2793f8ce0db9cd00f53e2f265dbea0561
Anik <adhikaryanik70@gmail.com>
2025-07-13 15:05:44 +0530

    feat: Update Navbar component with location selection and new icons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,13 +3,18 @@ import Cart from "../asset/shopping-cart.png";
 import theme from "../asset/themes.png";
 import man from "../asset/man.png";
 import location from "../asset/location.png";
+import notifiaction from "../asset/notification.png";
+import plus from "../asset/add.png";
 import { useState } from "react";
 const Navbar = () => {
-  const [dropdown, setDropdown] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState("");
 
-  const toggledropdown = () => {
-    setDropdown(!dropdown);
+  const locations = ["Delhi", "Mumbai", "Bangalore", "Kolkata", "Chennai"];
+
+  const handleChange = (e) => {
+    setSelectedLocation(e.target.value);
   };
+
   return (
     <nav className="w-full p-4 flex bg-[#9dc3e6]">
       <div className="container-fluid flex space-x-40 items-center">
@@ -19,28 +24,34 @@ const Navbar = () => {
         </div>
         <div className="flex items-center space-x-2">
           <input
-            className="form-control w-60 rounded-xl px-2 py-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="form-control w-60 rounded-2xl px-2 py-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn border border-green-500 text-green-500 rounded px-2 py-1 hover:bg-green-500 hover:text-white transition">
+          <button className="btn  text-green-500 rounded px-2 py-1 hover:text-white transition">
             <img src={SearchIcon} alt="search" className="w-4" />
           </button>
         </div>
       </div>
-      <div className="flex space-x-8 ml-auto items-center relative">
-        <div className="flex align-middle">
-        <img src={location} alt="location" className="w-8 cursor-pointer" onClick={toggledropdown} />
-        <h1>kolkata</h1>
+      <div className="flex space-x-12 ml-auto items-center relative">
+        <div className="flex items-center space-x-2">
+          <img src={location} alt="location" className="w-8 cursor-pointer" />
+          <select
+            value={selectedLocation}
+            onChange={handleChange}
+            className="p-1 border rounded-md w-24"
+          >
+            <option value="">Choose a location</option>
+            {locations.map((location, index) => (
+              <option key={index} value={location}>
+                {location}
+              </option>
+            ))}
+          </select>
         </div>
-        {dropdown && (
-          <ul className="absolute top-12 left-0 bg-gray-700 text-white list-none p-2 rounded shadow-lg z-50 min-w-[120px]">
-            <li className="py-1 px-3 hover:bg-gray-600 cursor-pointer">kolkata</li>
-            <li className="py-1 px-3 hover:bg-gray-600 cursor-pointer">kolkata</li>
-            <li className="py-1 px-3 hover:bg-gray-600 cursor-pointer">kolkata</li>
-          </ul>
-        )}
+        <img src={plus} alt="plus" className="w-8" />
+        <img src={notifiaction} alt="notifiaction" className="w-8" />
         <img src={theme} alt="theme" className="w-8" />
         <img src={man} alt="man" className="w-8" />
       </div>
