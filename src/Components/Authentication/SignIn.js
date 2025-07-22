d72762688bd852079fb174545ebfd2a6ed56bc9f
Anik <adhikaryanik70@gmail.com>
2025-07-22 11:19:59 +0530

    feat: Implement SignIn functionality with mock authentication and role-based navigation

--- src/Components/Authentication/SignIn.js
+++ src/Components/Authentication/SignIn.js
@@ -1,6 +1,38 @@
 import { Link } from "react-router-dom";
 import pic from "../../asset/pictures1.png";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import mockData from "../../asset/fakeApiResponce/mockdata2.json";
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+const handleLogin = async () => {
+  try {
+    const match = mockData.find(
+      (entry) => entry.email === email && entry.password === password
+    );
+
+    if (!match) throw new Error("Invalid credentials");
+
+    const { token, user } = match;
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("role", user.role);
+
+    if (user.role === "admin") {
+      navigate("/");
+    } else if (user.role === "user") {
+      navigate("/products");
+    } else {
+      alert("Unknown role");
+    }
+  } catch (err) {
+    alert("Login failed: " + err.message);
+  }
+};
+
   return (
     <div
       className={`flex items-center justify-around min-h-screen bg-white w-full space-x-4`}
@@ -28,6 +60,7 @@ const SignIn = () => {
               type="email"
               id="email"
               name="email"
+              onChange={(e) => setEmail(e.target.value)} placeholder="Email"
               required
               className="w-full px-3 py-2 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-xl"
             />
@@ -44,6 +77,7 @@ const SignIn = () => {
               id="password"
               name="password"
               required
+              onChange={(e) => setPassword(e.target.value)} placeholder="Password"
               className="w-full px-3 py-2 border rounded-3xl shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
@@ -51,6 +85,7 @@ const SignIn = () => {
             <button
               type="submit"
               className="w-28 py-2 font-semibold text-slate-400 bg-white shadow-xl rounded-3xl hover:bg-slate-200 items-center mt-4"
+              onClick={handleLogin}
             >
               Sign In
             </button>
