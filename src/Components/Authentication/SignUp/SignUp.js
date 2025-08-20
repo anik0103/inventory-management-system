4c7fca7d06a95cc1a35e8b35d2f9d82f3af152e9
Anik <adhikaryanik70@gmail.com>
2025-08-20 17:32:07 +0530

    feat: improve SignUp and SignUpForm components with enhanced layout and styling

--- src/Components/Authentication/SignUp/SignUp.js
+++ src/Components/Authentication/SignUp/SignUp.js
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import pic from "../../../asset/pictures1.png";
 import SignUpForm from "./SignUpForm";
 import { sendWelcomeEmail, notifyAdmin } from "./SignUpEmail";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
   const handleChange = (item) => {
@@ -32,8 +31,15 @@ const SignUp = () => {
       await sendWelcomeEmail(templateParams);
       await notifyAdmin(templateParams);
 
-      alert("Sign up successful! Please check your email for a welcome message.");
-      setFormData({ username: '', email: '', password: '', confirmPassword: '' });
+      alert(
+        "Sign up successful! Please check your email for a welcome message."
+      );
+      setFormData({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
     } catch (error) {
       console.error("Error during signup email process:", error);
       alert("Error sending emails. Please try again.");
@@ -41,13 +47,23 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex items-center justify-around min-h-screen bg-white w-full space-x-4">
-      <div className="mt-16">
-        <h1 className="text-[#bd78b5] text-4xl font-bold text-center">Welcome To Alpha Inventory</h1>
-        <img src={pic} alt="Logo" className="w-auto h-[30rem] mx-auto mb-4" />
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen w-full bg-white overflow-x-hidden">
+      <div className="w-full md:w-1/2 flex flex-col items-center md:justify-center text-center px-4">
+        <h1 className="text-[#bd78b5] text-3xl md:text-4xl font-bold mt-10 md:mt-12 mb-2">
+          Welcome To Alpha Inventory
+        </h1>
+        <img
+          src={pic}
+          alt="Logo"
+          className="w-full max-w-sm md:max-w-md lg:max-w-lg h-auto mx-auto mt-2"
+        />
       </div>
 
-      <SignUpForm formData={formData} handleChange={handleChange} handleSubmit={handleSubmit} />
+      <SignUpForm
+        formData={formData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 };
