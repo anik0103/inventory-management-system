9623be1e4f3f59c64f0276c4e83793c36ec66e40
Anik <adhikaryanik70@gmail.com>
2025-08-29 00:37:35 +0530

    create profile section and update navigation

---
+++ src/Components/Profile.js
@@ -0,0 +1,22 @@
+import { IoIosLogOut } from "react-icons/io";
+import { MdEdit } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <div className="icon-hover bg-slate-50 p-4 w-36 border rounded-md shadow-lg relative z-50">
+      <Link to="/app/userdetails" className="flex items-center gap-2 cursor-pointer hover:text-blue-500">
+        <MdEdit size={20} />
+        <p className="text-sm text-left">profile</p>
+      </Link>
+      <Link
+        to="/"
+        className="flex items-center gap-2 mt-2 hover:text-red-500 text-sm text-left"
+      >
+        <IoIosLogOut size={20} />
+        <span>Logout</span>
+      </Link>
+    </div>
+  );
+};
+export default Profile;
